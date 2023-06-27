dc3c08d1635fdb81f8d0690ac4535170c5d85c2f
Jérôme Dauge <jerome.dauge@necko.tech>
2023-06-27 16:27:34 +0200

    feat: initialize setup-smithy action

---
+++ src/cache.ts
@@ -0,0 +1,15 @@
+import * as core from "@actions/core";
+import { save } from "./utils";
+async function cleanup() {
+	const smithyBuildPath = core.getInput("smithy-build", {
+		required: false,
+		trimWhitespace: true,
+	});
+
+	if (smithyBuildPath) {
+		core.info("Save cache");
+		await save();
+	}
+}
+
+cleanup();
